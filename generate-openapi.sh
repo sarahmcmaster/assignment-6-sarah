87a01def268e6c6ecf35bc3fd43b04131d5278d9
sarbear28 <138033132+sarbear28@users.noreply.github.com>
2026-03-11 05:41:27 +0000

    fix openapi generation script line endings

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+set -e
+
 npx tsoa spec-and-routes
+
+if [ ! -f ./build/swagger.json ]; then
+  echo "swagger.json was not generated"
+  exit 1
+fi
+
 npx @openapitools/openapi-generator-cli generate -i ./build/swagger.json -o ./client -g typescript-fetch
